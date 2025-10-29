1a75461233396939fd3e36cbbc8405ccb5a43aa1
luciananadine055-stack <luciananadine055@gmail.com>
2025-10-29 20:31:31 -0300

    Create RL_datos.sql

---
+++ RL_datos.sql
@@ -0,0 +1,4 @@
+USE foodexpress;
+
+INSERT INTO pedidos (nombre, direccion, telefono, carrito, subtotal, fecha)
+VALUES ('Luciana Rojas', 'Av. Siempre Viva 742', '1122334455', '[{"nombre":"Clásica","precio":2500}]', 2500, NOW());
